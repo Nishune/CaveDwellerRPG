1fe57f4a8753057a69858ace67cc42973dc555b9
Rikard <rikardengstrom89@gmail.com>
2025-08-06 19:03:50 +0200

    Added the entrance class to the game. Testing functionality and learning.

--- src/com/nishune/cavegame/Game.java
+++ src/com/nishune/cavegame/Game.java
@@ -1,4 +1,24 @@
 package com.nishune.cavegame;
 
+import com.nishune.cavegame.model.Location;
+import com.nishune.cavegame.model.locations.Entrance;
+
 public class Game {
+    private Location currentLocation;
+
+    public Game() {
+        currentLocation = new Entrance();
+    }
+    public void start() {
+        System.out.println("You are at the " + currentLocation.getName());
+        System.out.println(currentLocation.getDescription());
+
+        currentLocation.onEnter();
+    }
+
+    public void movePlayer(String direction) {
+
+        currentLocation.onExit(direction);
+    }
+
 }
--- src/com/nishune/cavegame/Main.java
+++ src/com/nishune/cavegame/Main.java
@@ -1,4 +1,11 @@
 package com.nishune.cavegame;
 
 public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.start();
+
+        game.movePlayer("north");
+        game.movePlayer("exit");
+    }
 }
